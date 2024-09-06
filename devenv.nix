44d6ea9bce271306024091d143b823feb7387e1f
Drew Dalmedo <andrewdalmedo@gmail.com>
2024-09-06 18:06:55 -0400

    Clean up devenv file and add comments

--- devenv.nix
+++ devenv.nix
@@ -1,46 +1,41 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  # This devenv.nix file doesn't include many features of devenv. For a full list of features,
+  # see https://devenv.sh/
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep "2.42.0"
-  '';
-
+  # Languages you wish to use in development shells can be programmatically enabled: 
   languages.nix.enable = true;
+
+  # This will install all the necessary files needed to compile / run code written for
+  # that language. It will also install the industry-standard tooling for that language.
+  # For example, the following would not only install gcc, but also make:
+  #languages.c.enable = true;
+
+  # See the following links for more details:
+  # Documentation           ->  https://devenv.sh/languages/
+
+  # For the sake of time I chose to use javascript for this project,
+  # but devenv also supports using typescript.
   languages.javascript = {
     enable = true;
-    pnpm = {
-      enable = true;
-      install.enable = true;
+    pnpm = {  # pnpm is an implementation of npm with significantly faster performance
+      enable = true; 
+      install.enable = true; # auto-install all npm packages on entering a shell
     };
   };
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
+  # devenv has full dotenv integration, making the inclusion of dotenv in packages.json
+  # unnecessary. However, this integration is disabled for the sake of showing how converting to
+  # devenv may look when writing a configuration for an existing codebase without any rewrites
+  dotenv.enable = false;
 
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  # Processes and services are primarily used for starting things such as development servers, 
+  # SQL databases, etc. Since this example project is small, the only process here is for starting
+  # the Discord bot.
+  processes = {
+    bot.exec = "node ."; # when you run 'devenv up', the bot will start
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
