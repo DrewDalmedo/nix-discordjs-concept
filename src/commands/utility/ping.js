dccf3f8e199bbc818ae27e8ce5adbdcf0fb8a36f
Drew Dalmedo <andrewdalmedo@gmail.com>
2024-09-06 15:46:06 -0400

    Create basic example commands

---
+++ src/commands/utility/ping.js
@@ -0,0 +1,11 @@
+// taken from https://discordjs.guide/creating-your-bot/slash-commands.html#individual-command-files
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
